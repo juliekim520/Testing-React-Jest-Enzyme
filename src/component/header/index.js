34b1df7c9a7fdc650f34923b57379ab491436e0f
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-04-23 17:05:15 +1000

    Header Component and tests

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -3,10 +3,10 @@ import "./styles.scss";
 import Logo from "../../assets/graphics/logo.png";
 const Header = props => {
   return (
-    <header>
+    <header data-test="headerComponent">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Logo" />
+          <img data-test="logoIMG" src={Logo} alt="Logo" />
         </div>
       </div>
     </header>
