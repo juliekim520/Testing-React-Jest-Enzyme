55f51c75fe1cc68488473a2d160198722aaaae19
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-04-24 15:17:09 +1000

    tests for actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Header from "./component/header";
 import Headline from "./component/headline";
+import SharedButton from "./component/button";
+import ListItem from "./component/listItem";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions";
 import "./app.scss";
 
 const tempArr = [
@@ -14,7 +18,17 @@ const tempArr = [
 ];
 
 class App extends Component {
+  fetch = () => {
+    this.props.fetchPosts();
+  };
+
   render() {
+    const { posts } = this.props;
+
+    const configButton = {
+      buttonText: "Get Posts",
+      emitEvent: this.fetch
+    };
     return (
       <div className="App">
         <Header />
@@ -24,10 +38,32 @@ class App extends Component {
             desc="Click the button to render posts!"
             tempArr={tempArr}
           />
+          <SharedButton {...configButton} />
+          {posts.length > 0 && (
+            <div>
+              {posts.map((post, index) => {
+                const { title, body } = post;
+                const configListItem = {
+                  title,
+                  desc: body
+                };
+                return <ListItem key={index} {...configListItem} />;
+              })}
+            </div>
+          )}
         </section>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(App);
